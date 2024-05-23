fe1a39c1f491b7a47195aa43d60c4f8d4675825e
Vincent Schmitt <vincent.m.schmitt@me.com>
2024-05-23 14:45:35 +0200

    Optimization for the script and scene

--- Unity/UnityPandaProject/Assets/Scripts/TargetPlacement.cs
+++ Unity/UnityPandaProject/Assets/Scripts/TargetPlacement.cs
@@ -3,49 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Unity.Robotics.PickAndPlace
-{
+namespace Unity.Robotics.PickAndPlace {
 
     [RequireComponent(typeof(MeshRenderer))]
     [RequireComponent(typeof(BoxCollider))]
-    public class TargetPlacement : MonoBehaviour
-    {
+
+    public class TargetPlacement : MonoBehaviour {
+
+        public enum PlacementState {
+            Outside,
+            InsideFloating,
+            InsidePlaced
+        }
+
         const string k_NameExpectedTarget = "Target";
         static readonly int k_ShaderColorId = Shader.PropertyToID("_Color");
         // The threshold that the Target's speed must be under to be considered "placed" in the target area
         const float k_MaximumSpeedForStopped = 0.01f;
 
-        [SerializeField]
         [Tooltip("Target object expected by this placement area. Can be left blank if only one Target in scene")]
-        GameObject m_Target;
-        [SerializeField]
-        [Range(0, 255)]
+        [SerializeField] GameObject m_Target;
+        
         [Tooltip("Alpha value for any color set during state changes.")]
-        int m_ColorAlpha = 100;
-
-        MeshRenderer m_TargetMeshRenderer;
+        [Range(0, 255)]
+        [SerializeField] int m_ColorAlpha = 100;
 
         float m_ColorAlpha01 => m_ColorAlpha / 255f;
+        MeshRenderer m_TargetMeshRenderer;
         MeshRenderer m_MeshRenderer;
         BoxCollider m_BoxCollider;
         PlacementState m_CurrentState;
         PlacementState m_LastColoredState;
 
-        public PlacementState CurrentState
-        {
+        public PlacementState CurrentState {
             get => m_CurrentState;
-            private set
-            {
+            private set {
                 m_CurrentState = value;
                 UpdateStateColor();
             }
-        }
-
-        public enum PlacementState
-        {
-            Outside,
-            InsideFloating,
-            InsidePlaced
         }
 
         // Start is called before the first frame update
@@ -53,32 +48,27 @@ namespace Unity.Robotics.PickAndPlace
         {
             // Check for mis-configurations and disable if something has changed without this script being updated
             // These are warnings because this script does not contain critical functionality
-            if (m_Target == null)
-            {
+            if (m_Target == null) {
                 m_Target = GameObject.Find(k_NameExpectedTarget);
             }
 
-            if (m_Target == null)
-            {
+            if (m_Target == null) {
                 Debug.LogWarning($"{nameof(TargetPlacement)} expects to find a GameObject named " +
                     $"{k_NameExpectedTarget} to track, but did not. Can't track placement state.");
                 enabled = false;
                 return;
             }
 
-            if (!TrySetComponentReferences())
-            {
+            if (!TrySetComponentReferences()) {
                 enabled = false;
                 return;
             }
             InitializeState();
         }
 
-        bool TrySetComponentReferences()
-        {
+        bool TrySetComponentReferences() {
             m_TargetMeshRenderer = m_Target.GetComponent<MeshRenderer>();
-            if (m_TargetMeshRenderer == null)
-            {
+            if (m_TargetMeshRenderer == null) {
                 Debug.LogWarning($"{nameof(TargetPlacement)} expects a {nameof(MeshRenderer)} to be attached " +
                     $"to {k_NameExpectedTarget}. Cannot check bounds without it, so cannot track placement state.");
                 return false;
@@ -90,50 +80,39 @@ namespace Unity.Robotics.PickAndPlace
             return true;
         }
 
-        void OnValidate()
-        {
+        void OnValidate() {
             // Useful for visualizing state in editor, but doesn't wholly guarantee accurate coloring in EditMode
             // Enter PlayMode to see color update correctly
-            if (m_Target != null)
-            {
-                if (TrySetComponentReferences())
-                {
+            if (m_Target != null) {
+                if (TrySetComponentReferences()) {
                     InitializeState();
                 }
             }
         }
 
-        void InitializeState()
-        {
-            if (m_Target.GetComponent<BoxCollider>().bounds.Intersects(m_BoxCollider.bounds))
-            {
+        void InitializeState() {
+            if (m_Target.GetComponent<BoxCollider>().bounds.Intersects(m_BoxCollider.bounds)) {
                 CurrentState = IsTargetStoppedInsideBounds() ?
                     PlacementState.InsidePlaced : PlacementState.InsideFloating;
             }
-            else
-            {
+            else {
                 CurrentState = PlacementState.Outside;
             }
         }
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.name == m_Target.name)
-            {
+        void OnTriggerEnter(Collider other) {
+            if (other.gameObject.name == m_Target.name) {
                 CurrentState = PlacementState.InsideFloating;
             }
         }
 
-        void OnTriggerExit(Collider other)
-        {
-            if (other.gameObject.name == m_Target.name)
-            {
+        void OnTriggerExit(Collider other) {
+            if (other.gameObject.name == m_Target.name) {
                 CurrentState = PlacementState.Outside;
             }
         }
 
-        bool IsTargetStoppedInsideBounds()
-        {
+        bool IsTargetStoppedInsideBounds() {
             var targetIsStopped = m_Target.GetComponent<Rigidbody>().velocity.magnitude < k_MaximumSpeedForStopped;
             var targetIsInBounds = m_BoxCollider.bounds.Contains(m_TargetMeshRenderer.bounds.center);
 
@@ -141,26 +120,21 @@ namespace Unity.Robotics.PickAndPlace
         }
 
         // Update is called once per frame
-        void Update()
-        {
-            if (CurrentState != PlacementState.Outside)
-            {
+        void Update() {
+            if (CurrentState != PlacementState.Outside) {
                 CurrentState = IsTargetStoppedInsideBounds() ?
                     PlacementState.InsidePlaced : PlacementState.InsideFloating;
             }
         }
 
-        void UpdateStateColor()
-        {
-            if (m_CurrentState == m_LastColoredState)
-            {
+        void UpdateStateColor() {
+            if (m_CurrentState == m_LastColoredState) {
                 return;
             }
 
             var mpb = new MaterialPropertyBlock();
             Color stateColor;
-            switch (m_CurrentState)
-            {
+            switch (m_CurrentState) {
                 case PlacementState.Outside:
                     stateColor = Color.red;
                     break;
