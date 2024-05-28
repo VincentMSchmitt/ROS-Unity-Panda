5a0b6a91885468e4b42638b70086a5663d0e6555
Vincent Schmitt <vincent.m.schmitt@me.com>
2024-05-28 21:53:04 +0200

    add comments for FK

--- Unity/UnityPandaProject/Assets/Scripts/DirectKinematics.cs
+++ Unity/UnityPandaProject/Assets/Scripts/DirectKinematics.cs
@@ -23,26 +23,27 @@ public class DirectKinematics {
         Matrix4x4 T_7F  = TFMatrix(7, dhParameters);
         Matrix4x4 T_FEE = TFMatrix(8, dhParameters);
 
-        // TODO: THIS is always Zero-Matrix, why?
+        // TODO: check if this kinematics chain is calculated correctly
+        // calculate kinematic chain for the EE transformation matrix
         Matrix4x4 T_EE = T_01 * T_12 * T_23 * T_34 * T_45 * T_56 * T_67 * T_7F * T_FEE;
-        //LogMatrix("TCP-TF", T_EE);
 
         return T_EE;
     }
 
     static float[,] DHParams(float[] jointAngles) {
         // DH-Parameter: https://www.researchgate.net/publication/357238256_Analytical_Inverse_Kinematics_for_Franka_Emika_Panda_-_a_Geometrical_Solver_for_7-DOF_Manipulators_with_Unconventional_Design
+        // DH-Parameters: alpha = link twist, a = link length, d = offset, theta = joint angle
         float[,] dh = new float[,] {
-            //     alpha      a (m)    d (m)      theta
-            {           0,        0,  0.333f, jointAngles[0]}, // joint 1
-            {   -M_PI / 2,        0,       0, jointAngles[1]}, // joint 2
-            {    M_PI / 2,        0,  0.316f, jointAngles[2]}, // joint 3
-            {    M_PI / 2,  0.0825f,       0, jointAngles[3]}, // joint 4
-            {   -M_PI / 2, -0.0825f,  0.384f, jointAngles[4]}, // joint 5
-            {    M_PI / 2,        0,       0, jointAngles[5]}, // joint 6
-            {    M_PI / 2,   0.088f,       0, jointAngles[6]}, // joint 7
-            {           0,        0,  0.107f,              0}, // flanch (F)
-            {           0,        0, 0.1034f,       M_PI / 4}  // end effector (EE)
+        //   alpha(rad)    a(m)     d(m)     theta(rad)
+            {        0,        0,  0.333f, jointAngles[0]}, // joint 1
+            {-M_PI / 2,        0,       0, jointAngles[1]}, // joint 2
+            { M_PI / 2,        0,  0.316f, jointAngles[2]}, // joint 3
+            { M_PI / 2,  0.0825f,       0, jointAngles[3]}, // joint 4
+            {-M_PI / 2, -0.0825f,  0.384f, jointAngles[4]}, // joint 5
+            { M_PI / 2,        0,       0, jointAngles[5]}, // joint 6
+            { M_PI / 2,   0.088f,       0, jointAngles[6]}, // joint 7
+            {        0,        0,  0.107f,              0}, // flanch (F)
+            {        0,        0, 0.1034f,       M_PI / 4}  // end effector (EE)
         };
         return dh;
     }
@@ -55,6 +56,11 @@ public class DirectKinematics {
         float theta = dh[i, 3];
 
         // standard DH-matrix for a 3D pose with the parameters: alpha, a, d, theta
+        // Each row of the matrix is set according to DH conventions:
+        //  * First row:  Sets the values for the rotation around the Z-axis and the shift along the X-axis.
+        //  * Second row: Sets the values for the rotation around the Z-axis and the shift along the Y-axis.
+        //  * Third line: Sets the values for the displacement along the Z-axis.
+        //  * Fourth row: Sets the homogeneity condition of the matrix.
         Matrix4x4 TF = Matrix4x4.identity;
         TF.SetRow(0, new Vector4(Mathf.Cos(theta),  -Mathf.Sin(theta) * Mathf.Cos(alpha),  Mathf.Sin(theta) * Mathf.Sin(alpha), a * Mathf.Cos(theta)));
         TF.SetRow(1, new Vector4(Mathf.Sin(theta),   Mathf.Cos(theta) * Mathf.Cos(alpha), -Mathf.Cos(theta) * Mathf.Sin(alpha), a * Mathf.Sin(theta)));
@@ -62,10 +68,5 @@ public class DirectKinematics {
         TF.SetRow(3, new Vector4(               0,                                     0,                                    0,                    1));
 
         return TF;
-    }
-
-    public static void LogMatrix(string matrixName, Matrix4x4 matrix) {
-        Debug.Log(matrixName + ":");
-        Debug.Log(matrix.ToString("F4"));
     }
 }
