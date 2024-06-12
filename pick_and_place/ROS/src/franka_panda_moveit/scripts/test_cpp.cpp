02bdfc88cd8791bdff9de16db6501ea760a51661
Vincent Schmitt <vincent.m.schmitt@me.com>
2024-06-12 11:58:05 +0200

    added moveit visual tools

--- pick_and_place/ROS/src/franka_panda_moveit/scripts/test_cpp.cpp
+++ pick_and_place/ROS/src/franka_panda_moveit/scripts/test_cpp.cpp
@@ -2,6 +2,17 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <geometry_msgs/Pose.h>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "test_example");
     ros::NodeHandle node_handleh;
