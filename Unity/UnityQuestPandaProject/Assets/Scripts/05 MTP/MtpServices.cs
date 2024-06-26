762c87fbb0918c015c322ff8b902b148121948d9
Vincent Schmitt <vincent.m.schmitt@me.com>
2024-06-26 15:37:28 +0200

    added asyn functionality to wait for the compleation of the robot movement

--- Unity/UnityQuestPandaProject/Assets/Scripts/05 MTP/MtpServices.cs
+++ Unity/UnityQuestPandaProject/Assets/Scripts/05 MTP/MtpServices.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 
@@ -10,7 +11,7 @@ using RosMessageTypes.FrankaPandaCommunication;
 
 namespace Panda.MTP {
     enum MoveServiceType { Arm = 1, Hand = 2 }
-    public class MoveService : MonoBehaviour {
+    public class MtpServices : MonoBehaviour {
         [Tooltip("Percentage of the max speed")] public float speed = 1f;
         private string rosServiceName = "move_service";
         private ROSConnection ros;
@@ -24,19 +25,23 @@ namespace Panda.MTP {
             ros.ImplementService<MoveServiceRequest, MoveServiceResponse>(rosServiceName, ServiceRequestHandler);
         }
 
-        private MoveServiceResponse ServiceRequestHandler(MoveServiceRequest request) {
+        private async Task<MoveServiceResponse> ServiceRequestHandler(MoveServiceRequest request) {
+            var response = new MoveServiceResponse();
             if (request.trajectory != null) {
                 Debug.Log("Move trajectory request received from " + rosServiceName + ".");
-                StartCoroutine(ExecuteTrajectories(request));
-            }
-            else {
+                await ExecuteTrajectoriesAsync(request);
+                response.success = true;
+            } else {
                 Debug.LogError("No trajectory returned from MoveService.");
+                response.success = false;
             }
-
-            // Create and return the response
-            MoveServiceResponse response = new MoveServiceResponse();
-            response.success = true;
             return response;
+        }
+
+        private Task ExecuteTrajectoriesAsync(MoveServiceRequest request) {
+            var tcs = new TaskCompletionSource<bool>();
+            StartCoroutine(ExecuteTrajectories(request, () => tcs.SetResult(true)));
+            return tcs.Task;
         }
 
         private IEnumerator WaitForAllCoroutines(List<Coroutine> coroutines) {
@@ -45,10 +50,13 @@ namespace Panda.MTP {
             }
         }
 
-        private IEnumerator ExecuteTrajectories(MoveServiceRequest request) {
+        private IEnumerator ExecuteTrajectories(MoveServiceRequest request, System.Action onComplete) {
             RobotController robotController = RobotController.GetInstance;
+            // cast uint8 into trajectoryType
+            MoveServiceType trajectoryType = (MoveServiceType)request.trajectory_type;
 
-            if (request.trajectory != null) {
+            // check which kind of trajectory is sent and act accordingly
+            if (trajectoryType == MoveServiceType.Arm) {
                 foreach (var point in request.trajectory.joint_trajectory.points) {
                     var jointPositions = point.positions;
                     var result = jointPositions.Select(r => (float)r * Mathf.Rad2Deg).ToArray();
@@ -63,6 +71,26 @@ namespace Panda.MTP {
                     yield return StartCoroutine(WaitForAllCoroutines(jointCoroutines));
                 }
             }
+
+            if (trajectoryType == MoveServiceType.Hand) {
+                foreach (var point in request.trajectory.joint_trajectory.points) {
+                    var jointPositions = point.positions;
+                    var result = jointPositions.Select(r => (float)r * Mathf.Rad2Deg).ToArray();
+
+                    Debug.LogWarning("Moving a hand joint is not implemented yet.");
+
+                    // // coroutines for joints movements
+                    // List<Coroutine> jointCoroutines = new List<Coroutine>();
+                    // List<IMoveCommand> handJoints = robotController.GetHandJoints();
+                    // for (int i = 0; i < jointPositions.Length; ++i) {
+                    //     jointCoroutines.Add(StartCoroutine(handJoints[i].MoveToTarget(result[i], speed)));
+                    // }
+                    // // wait, until every joint is where he is supposed to be
+                    // yield return StartCoroutine(WaitForAllCoroutines(jointCoroutines));
+                }
+            }
+
+            onComplete?.Invoke();
         }
     }
 }
